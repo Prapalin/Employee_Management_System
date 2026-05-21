cc14f964686411aea0f063d14e8104d17af13fba
Prapalin <mishraprapalin@gmail.com>
2026-05-21 17:38:14 +0530

    AllTask page added

--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 
 const AllTask = () => {
   return (
-    <div className='bg-[#1c1c1c] p-5'>
-      <div className='bg-red-300 py-2 px-4'>
-        <h2>Prapalin</h2>
+    <div id='AllTask' className='bg-[#1c1c1c] p-5 mt-5 rounded h-40 overflow-auto'>
+      <div className='text-white mb-2 flex justify-between rounded bg-yellow-400 py-2 px-4'>
+        <h1>Prapalin</h1>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className='text-white mb-2 flex justify-between rounded bg-red-400 py-2 px-4'>
+        <h1>Prapalin</h1>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className='text-white mb-2 flex justify-between rounded bg-emerald-400 py-2 px-4'>
+        <h1>Prapalin</h1>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className='text-white mb-2 flex justify-between rounded bg-purple-400 py-2 px-4'>
+        <h1>Prapalin</h1>
+        <h3>Make a UI design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className='text-white mb-2 flex justify-between rounded bg-blue-400 py-2 px-4'>
+        <h1>Prapalin</h1>
         <h3>Make a UI design</h3>
         <h5>Status</h5>
       </div>
